abb19986c5e1c912f2f0ba25349431e2e1c24723
Guillaume BILLEY <guillaume@marmelab.com>
2025-07-22 11:00:31 +0200

    feat: add the ability to choose the storage

--- packages/ra-apisix-oidc/src/httpClient.ts
+++ packages/ra-apisix-oidc/src/httpClient.ts
@@ -1,7 +1,7 @@
 import { fetchUtils, type Options } from "ra-core";
 
-export const httpClient = (url: string, options: Options = {}) => {
-  const token = localStorage.getItem("access_token");
+export const httpClient = (storage: Storage = localStorage) => (url: string, options: Options = {}) => {
+  const token = storage.getItem("access_token");
   return fetchUtils.fetchJson(url, {
     ...options,
     user: { authenticated: !!token, token: `Bearer ${token}` },
